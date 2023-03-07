a1af738f7bc3c7203be78571c86933923f1987ae
bob_140 <bob_140@ngs.ru>
2023-03-07 13:37:21 +0700

    formatted

--- config/config.php
+++ config/config.php
@@ -2,33 +2,32 @@
 
 /**
  * файл настроек
- * 
+ *
  */
 
 //> константы для обращения к контроллеру
-define ('PATH_PREFIX', '../controllers/');
-define ('PATH_POSTFIX', 'Controller.php');
+define('PATH_PREFIX', '../controllers/');
+define('PATH_POSTFIX', 'Controller.php');
 
-define ('QUANTITY_PER_PAGE', 10);
-define ('QUANTITY_PAGINATOR', 5);
+define('QUANTITY_PER_PAGE', 10);
+define('QUANTITY_PAGINATOR', 5);
 //<
 
-
-define ('DEFOLT_STEP', 1000);
-define ('REST_KEY', '12345');
+define('DEFOLT_STEP', 1000);
+define('REST_KEY', '12345');
 
 $userGroupSt = array('1', '4');
 $userGroupMd = array('1', '2', '4');
 $userGroupLg = array('1', '2', '3', '4');
 
 //> картинки по умолчанию
-$imageDefCat  = '0.jpg'; // 260 x 260
+$imageDefCat = '0.jpg'; // 260 x 260
 $imageDefProd = '0.jpg'; // 260 x 260, md -340 x 340, lg - 800 x 800
 //<
 
 $lgLen = 50;
 $mdLen = 30;
-$smLen = 20;  
+$smLen = 20;
 
 $website = 'http://beautic.local';
 $mailAdmin = 'bob_140@ngs.ru';
@@ -39,10 +38,10 @@ $template = 'default';
 $templateAdmin = 'admin';
 
 // пути к файлам шаблонов (*.tpl)
-define ('TEMPLATE_PREFIX', "../views/{$template}/");
-define ('TEMPLATE_ADMIN_PREFIX', "../views/{$templateAdmin}/");
+define('TEMPLATE_PREFIX', "../views/{$template}/");
+define('TEMPLATE_ADMIN_PREFIX', "../views/{$templateAdmin}/");
 
-define ('TEMPLATE_POSTFIX', '.tpl');
+define('TEMPLATE_POSTFIX', '.tpl');
 
 // пути к файлам шаблонов в вебпространстве
 define('TEMPLATE_WEB_PATH', "/templates/{$template}/");
@@ -51,7 +50,7 @@ define('TEMPLATE_ADMIN_WEB_PATH', "/templates/{$templateAdmin}/");
 
 //> инициализация шаблона смарти
 // put full path to Smarty.class.php
-require('../library/smarty/libs/Smarty.class.php');
+require '../library/smarty/libs/Smarty.class.php';
 $smarty = new Smarty();
 
 $smarty->setTemplateDir(TEMPLATE_PREFIX);
